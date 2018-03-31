d776a98ca3cc4de70714d60acfb42e29f57d7c8a
Sanni Michael Tomiwa <tomiwatech@gmail.com>
2018-03-31 23:01:20 +0100

    Initial commit to states in nigeria

---
+++ README.md
@@ -0,0 +1,9 @@
+# States in nigeria 
+
+- Reuse states in nigeria
+- Access states in nigeria in a json format
+- Share and Share
+
+
+
+# Opensource
---
+++ states.js
@@ -0,0 +1,92 @@
+/**
+ 
+    AN ARRAY OF STATES IN NIGERIA
+
+ **/
+
+let states_in_nigeria = [
+    "Abia",
+    "Adamawa",
+    "Anambra",
+    "Akwa Ibom",
+    "Bauchi",
+    "Bayelsa",
+    "Benue",
+    "Borno",
+    "Cross River",
+    "Delta",
+    "Ebonyi",
+    "Enugu",
+    "Edo",
+    "Ekiti",
+    "FCT - Abuja",
+    "Gombe",
+    "Imo",
+    "Jigawa",
+    "Kaduna",
+    "Kano",
+    "Katsina",
+    "Kebbi",
+    "Kogi",
+    "Kwara",
+    "Lagos",
+    "Nasarawa",
+    "Niger",
+    "Ogun",
+    "Ondo",
+    "Osun",
+    "Oyo",
+    "Plateau",
+    "Rivers",
+    "Sokoto",
+    "Taraba",
+    "Yobe",
+    "Zamfara"
+  ]
+
+
+/**
+ 
+    AN ARRAY OF OBJECTS OF STATES AND THEIR CAPITALS IN NIGERIA
+
+ **/
+
+let states_and_capitals_in_nigeria = [
+    { state : "Abia", capital : "Umuahia" },
+    { state: "Adamawa", capital: "Yola" },
+    { state : "Akwa Ibom ", capital : "Uyo" },
+    { state: "Anambra", capital: "Akwa" },
+    { state : "Bauchi", capital : "Bauchi" },
+    { state: "Bayelsa", capital: "Yenogoa" },
+    { state : "Benue", capital : "Makurdi" },
+    { state: "Borno", capital: "Maiduguri" },
+    { state : "Cross River", capital : "Calabar" },
+    { state: "Delta", capital: "Asaba" },
+    { state : "Edo", capital : "Benin" },
+    { state: "Ebonyi", capital: "Abakaliki" },
+    { state : "Ekiti", capital : "Ado Ekiti" },
+    { state: "Enugu", capital: "Enugu" },
+    { state : "Gombe", capital : "Gombe" },
+    { state: "Imo", capital: "Owerri" },
+    { state : "Jigawa", capital : "Dutse" },
+    { state: "Kaduna", capital: "Kaduna" },
+    { state : "Kano", capital : "Kano" },
+    { state: "Katsina", capital: "Katsina" },
+    { state : "Kebbi", capital : "Bernin Kebbi" },
+    { state: "Kogi", capital: "Lokoja" },
+    { state : "Kwara", capital : "Ilorin" },
+    { state: "Lagos", capital: "Ikeja" },
+    { state : "Nasarawa", capital : "Lafia" },
+    { state: "Niger", capital: "Minna" },
+    { state : "Ogun", capital : "Abeokuta" },
+    { state: "Ondo", capital: "Akure" },
+    { state : "Osun", capital : "Osogbo" },
+    { state: "Oyo", capital: "Ibadan" },
+    { state : "Plateau", capital : "Jos" },
+    { state: "Rivers", capital: "Port Harcourt" },
+    { state : "Sokoto", capital : "Sokoto" },
+    { state: "Taraba", capital: "Jalingo" },
+    { state : "Yobe", capital : "Damaturu" },
+    { state: "Zamfara", capital: "Gusau" },
+    { state : "FCT", capital : "Abuja" }
+]
